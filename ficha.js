72e50c2bef73c7531148e0e8cb6c74c2e3447ea4
Nandw012009 <oliveira.pereira.fernanda@escola.pr.gov.br>
2026-06-30 11:49:44 +0000

    v2

---
+++ ficha.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const camisa = {
+    tamanho: 14, //number
+    tecido:"Algodão",
+    design: true,
+    cor: "branco"
+    marca: "hering"
+};
+
+//console.log(camiseta);
+console.log(``)
